aaa4bc72d0af2290d4d03e83a813f3c966fb6881
isavieira121245 <visadora178@gmail.com>
2023-11-21 19:00:33 +0000

    save1

--- provao1/src/componentes/SimpleCounter.js
+++ provao1/src/componentes/SimpleCounter.js
@@ -9,4 +9,27 @@ function contador(){
        );
 }
 
-export default contador;
+import React, { useState } from 'react';
+import './SimpleCounter.css';
+
+const SimpleCounter = () => {
+  const [contador, setContador] = useState(0);
+
+    const adicionar = () => {
+        setContador(contador + 1);
+          };
+
+            const subtrair = () => {
+                setContador(contador - 1);
+                  };
+
+                    return (
+                        <div className="simple-counter">
+                              <h2>Contador: {contador}</h2>
+                                    <button onClick={adicionar}>Incrementar</button>
+                                          <button onClick={subtrair}>Decrementar</button>
+                                              </div>
+                                                );
+                                                };
+
+                                                export default SimpleCounter;
