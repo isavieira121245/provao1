18c8a6ad56d17dbb9a08ac22a2c465e436c5200f
isavieira121245 <125272651+isavieira121245@users.noreply.github.com>
2023-11-21 16:42:50 -0300

    Atualizar o SimpleCounter.js

--- provao1/src/componentes/SimpleCounter.js
+++ provao1/src/componentes/SimpleCounter.js
@@ -4,22 +4,19 @@ import './SimpleCounter.css';
 
 const SimpleCounter = () => {
   const [contador, setContador] = useState(0);
+const adicionar = () => {
+  setContador(contador + 1);
+  };
+  const subtrair = () => {
+   setContador(contador - 1);
+   };
 
-    const adicionar = () => {
-        setContador(contador + 1);
-          };
-
-            const subtrair = () => {
-                setContador(contador - 1);
-                  };
-
-                    return (
-                        <div className="simple-counter">
-                              <h2>Contador: {contador}</h2>
-                                    <button onClick={adicionar}>Incrementar</button>
-                                          <button onClick={subtrair}>Descrementar</button>
-                                              </div>
-                                                );
-                                                };
+return (  
+<div className="simple-counter">
+                             
+ <h2>Contador: {contador}</h2>
+ <button onClick={adicionar}>Incrementar</button>  <button onClick={subtrair}>Descrementar</button> </div>
+ );
+ };
 
                                                 export default SimpleCounter;
