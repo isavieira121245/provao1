0863d41fb3c79084544e80c6765ad52c4e7cfc8a
isavieira121245 <visadora178@gmail.com>
2023-11-21 17:39:04 +0000

    save1

--- provao1/src/App.js
+++ provao1/src/App.js
@@ -1,18 +1,19 @@
-
 import React, {useState} from 'react'
 import './App.css';
-
+ 
 function App() {
-
+ 
   const [contador, setContador] = useState(0);
+ 
   return (
     <div className="App">
-        </contador>
+    <p>Você clicou {contador} vezes </p>
+    <button onClick={() => setContador(contador + 1)}>
+clique +1
+ 
+    </button>
+ 
     </div>
   );
-
-
- 
 }
-
 export default App;
